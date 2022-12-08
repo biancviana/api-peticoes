90b6f5581cfab94d28123ec7af3f2c5eef7e36ab
Bianca Viana <bvitoriaviana@gmail.com>
2022-12-07 21:51:31 -0300

    Corrijo erro para rodar os testes automatizados

--- __test__/peticoes.test.js
+++ __test__/peticoes.test.js
@@ -1,11 +1,12 @@
 const supertest = require('supertest');
 const app = require('../index');
 peticao = "";
+token = "";
 
 // para buscar todas as petições
-describe('peticao', () => {
+describe('peticoes', () => {
   describe("Rotas petições", () => {
-    describe('Consulta de todas as petições', () => {
+    describe('Para consultar todas as petições', () => {
       it("Deve retornar o status 200", async () => {
         await supertest(app)
           .get(`/api/peticoes`)
@@ -15,75 +16,90 @@ describe('peticao', () => {
   })
 });
 
-// para inserir uma petição - sendo um usuário autenticado
-describe('peticao', () => {
-  describe("Rotas petições", () => {
-    describe('Inserir uma petição sendo um usuário autenticado', () => {
-      it("Deve retornar o status 200", async () => {
+// para autenticar o usuário
+describe('authUser', () => {
+  describe('Rotas petições', () => {
+    describe('Para autentifcar o usuário', () => {
+      it('Deve retornar o status 200', async () => {
+        await supertest(app)
+          .post(`/api/authUser`)
+          .send({ email: 'bianca@gmail.com', senha: 'bianca' })
+          .expect(200)
+          .then((rest) => {
+            token = rest.body;
+          })
+      })
+    })
+  })
+});
+
+
+// para inserir uma petição - NÃO sendo um usuário autenticado
+describe('CRUD', () => {
+  describe('Rotas petições', () => {
+    describe('Para inserir uma petição - usuário não autenticado', () => {
+      it('Deve retornar o status 401', async () => {
         await supertest(app)
           .post(`/api/peticoes`)
-          .set('x-access-token', token = "")
           .send({ titulo: 'Título para teste automatizado', descricao: 'Descrição para teste automatizado', autor: 'Autor para teste automatizado', fotoPeticao: 'fotoPeticao para teste automatizado' })
-          .expect(200);
-      })
+          .expect(401);
+      });
     })
   })
 });
 
-// para inserir uma petição - NÃO sendo um usuário autenticado
-describe('peticao', () => {
-  it('Deve retornar o status 401', async () => {
-    await supertest(app)
-      .post(api)
-      .send({ titulo: 'Título para teste automatizado', descricao: 'Descrição para teste automatizado', autor: 'Autor para teste automatizado', fotoPeticao: 'fotoPeticao para teste automatizado' })
-      .expect(401);
+// para editar uma petição que não existe (ID errado) - sendo um usuário autenticado
+describe('CRUD', () => {
+  describe('Rotas petições', () => {
+    describe('Para editar uma petição que não existe - usuário autenticado', () => {
+      it('Deve retornar o status 401', async () => {
+        await supertest(app).put(`${`/api/peticoes`}/1234`).set('x-access-token', token)
+          .send({ titulo: 'Título para teste automatizado 3', descricao: 'Descrição para teste automatizado', autor: 'Autor para teste automatizado', fotoPeticao: 'fotoPeticao para teste automatizado' })
+          .expect(401);
+      });
+    });
   });
 });
 
-// para editar uma petição - sendo um usuário autenticado
-describe('peticao', () => {
-  describe("Rotas petições", () => {
-    describe('Editar uma petição sendo um usuário autenticado', () => {
-      it("Deve retornar o status 200", async () => {
+
+// para excluir uma petição que não existe (ID errado) - sendo um usuário autenticado
+describe('CRUD', () => {
+  describe('Rotas petições', () => {
+    describe('Para excluir uma petição que não existe - usuário autenticado', () => {
+      it('Deve retornar o status 401', async () => {
         await supertest(app)
-          .put(`/api/peticoes` + peticao)
-          .set('x-access-token', token = "")
-          .send({ titulo: 'Título para teste automatizado', descricao: 'Descrição para teste automatizado', autor: 'Autor para teste automatizado', fotoPeticao: 'fotoPeticao para teste automatizado' })
-          .expect(200);
-      })
-    })
-  })
-});
-
-// para inserir uma petição - NÃO sendo um usuário autenticado
-describe('peticoes', () => {
-  it('Deve retornar o status 404', async () => {
-    await supertest(app)
-      .put(`${'/api/peticoes'}/638cff4d777e8bc47b4486bb`)
-      .send({ titulo: 'Título para teste automatizado', descricao: 'Descrição para teste automatizado', autor: 'Autor para teste automatizado', fotoPeticao: 'fotoPeticao para teste automatizado' })
-      .expect(404);
+          .delete(`${`/api/peticoes`}/1234`)
+          .set('x-access-token', token)
+          .expect(401);
+      });
+    });
   });
 });
 
-// para excluir uma petição - sendo um usuário autenticado
-describe('peticao', () => {
-  describe("Rotas petições", () => {
-    describe('Excluir uma petição sendo um usuário autenticado', () => {
-      it("Deve retornar o status 200", async () => {
+// para assinar uma petição que não existe (ID errado) - sendo um usuário autenticado
+describe('signUser', () => {
+  describe('Rotas petições', () => {
+    describe('Para assinar uma petição que não existe - usuário autenticado', () => {
+      it('Deve retornar o status 401', async () => {
         await supertest(app)
-          .delete(`${'/api/peticoes'}/` + peticao)
-          .set('x-access-token', token = "")
-          .expect(200);
-      })
-    })
-  })
+          .post(`${`/api/signPeticoes`}/1234`)
+          .set('x-access-token', token)
+          .expect(401);
+      });
+    });
+  });
 });
 
-// para excluir uma petição - NÃO sendo um usuário autenticado
-describe('peticoes', () => {
-  it('Deve retornar o status 404', async () => {
-    await supertest(app)
-      .put(`${'/api/peticoes'}/638cff4d777e8bc47b4486bb`)
-      .expect(404);
-  });
+// para adicionar um comentário na petição - NÃO sendo um usuário autenticado
+describe('Funcionalidade a mais', () => {
+  describe('Rotas petições', () => {
+    describe('Para adicionar um comentário na petição', () => {
+      it('Deve retornar o status 200', async () => {
+        await supertest(app)
+          .post(`/api/addTopic/638cff4d777e8bc47b4486bb`)
+          .send({ comentarios: 'Comentário teste' })
+          .expect(200);
+      });
+    })
+  })
 });
